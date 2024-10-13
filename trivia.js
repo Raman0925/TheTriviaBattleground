bd24e919c85a5db8833c824117efe82dbadc8901
Raman0925 <ramankumarkannaujiya@gmail.com>
2024-10-13 19:18:32 +0530

    Added category features and JS functionality to choose category after the game.

--- trivia.js
+++ trivia.js
@@ -2,9 +2,21 @@ let player1;
 let player2;
 let currentPlayer;
 let playerScores = {};
-  
+let selectedCategories = [];
 let quizData = [];
 let categories;
+const categoriesArray = [
+  "food_and_drink",
+  "arts_and_literature",
+  "film_and_tv",
+  "general_knowledge",
+  "geography",
+  "history",
+  "music",
+  "science",
+  "society_and_culture",
+  "sport_and_leisure",
+];
 
 let questionIndex = 0;
 
@@ -59,6 +71,31 @@ const fetchingQuestions = async (categories) => {
   }
 };
 
+const removingCategories = () => {
+  const endForm = document.getElementById("endForm");
+  endForm.innerHTML = "";
+
+  const modifiedCategories = document.getElementById("modifiedCategories");
+
+  modifiedCategories.innerHTML = "";
+
+  filteredCategories = categoriesArray.filter(
+    (category) => !selectedCategories.includes(category)
+  );
+
+  filteredCategories.forEach((element) => {
+    const option = document.createElement("option");
+    option.text = element.toUpperCase().replace(/_/g, " ");
+    option.value = element;
+    modifiedCategories.appendChild(option);
+  });
+
+  const nextCategoryButton = document.createElement("button");
+  nextCategoryButton.textContent = "Select Another Category";
+  nextCategoryButton.onclick = afterNextCategory;
+  endForm.append(nextCategoryButton);
+};
+
 const displayFeedback = (message) => {
   const feedbackDiv = document.getElementById("feedback");
   feedbackDiv.textContent = message;
@@ -70,11 +107,15 @@ const updateScoreDisplay = () => {
 
   // Create score elements for both players
   const player1ScoreElement = document.createElement("div");
-  player1ScoreElement.textContent = `${player1}: ${playerScores[player1]}`;
+  player1ScoreElement.textContent = `${
+    player1.charAt(0).toUpperCase() + player1.slice(1)
+  }: ${playerScores[player1]}`;
   player1ScoreElement.classList.add("score-item"); // Optional styling class
 
   const player2ScoreElement = document.createElement("div");
-  player2ScoreElement.textContent = `${player2}: ${playerScores[player2]}`;
+  player2ScoreElement.textContent = `${
+    player2.charAt(0).toUpperCase() + player1.slice(1)
+  }: ${playerScores[player2]}`;
   player2ScoreElement.classList.add("score-item"); // Optional styling class
 
   // Append score elements to the score container
@@ -83,9 +124,9 @@ const updateScoreDisplay = () => {
 };
 
 const checkAnswer = (selectedAnswer, correctAnswer, level, currentPlayer) => {
-  const points = { easy: 10, medium: 15, hard: 20 };
+  const POINTS = { easy: 10, medium: 15, hard: 20 };
   if (selectedAnswer === correctAnswer) {
-    playerScores[currentPlayer] += points[level];
+    playerScores[currentPlayer] += POINTS[level];
     console.log(playerScores);
     displayFeedback(
       `Correct answer!  score is now: ${playerScores[currentPlayer]}`
@@ -103,7 +144,9 @@ const questionRender = (question, currentPlayer) => {
   battleGround.innerHTML = "";
   const playerNameTag = document.createElement("p");
   playerNameTag.classList.add("player-name");
-  playerNameTag.textContent = `Player: ${currentPlayer}`;
+  playerNameTag.textContent = `Player: ${
+    currentPlayer.charAt(0).toUpperCase() + currentPlayer.slice(1)
+  }`;
   battleGround.appendChild(playerNameTag);
 
   const questionDiv = document.createElement("div");
@@ -137,28 +180,43 @@ const questionRender = (question, currentPlayer) => {
   });
   battleGround.appendChild(questionDiv);
 };
+
 const askNextQuestion = () => {
   if (questionIndex < quizData.length) {
     const question = quizData[questionIndex];
     currentPlayer = questionIndex % 2 === 0 ? player1 : player2;
-
     questionRender(question, currentPlayer);
   } else {
-    displayFeedback(
-      `Game over! Final scores - ${player1}: ` +
-        playerScores[player1]+
-        `,  ${player2}: ` +
-        playerScores[player2]
-    );
+    endGame();
   }
 };
+
+const endGame = () => {
+  const battleGround = document.getElementById("battle-ground");
+  const feedbackDiv = document.getElementById("feedback");
+  feedbackDiv.classList.add("feedback-flex");
+  battleGround.classList.add("battle-ground-h");
+  battleGround.classList.remove("battle-ground");
+  removingCategories();
+
+  const endScreen = document.getElementById("endGame");
+  endScreen.classList.remove("next-categories-h");
+  endScreen.classList.add("next-categories");
+
+  displayFeedback(
+    `Game over! Final scores - ${player1}: ${playerScores[player1]}, ${player2}: ${playerScores[player2]}`
+  );
+
+  // Task 3 Create a button to allow selecting another category
+};
+
 const startGameHandler = (e) => {
   e.preventDefault();
 
   player1 = document.getElementById("player-1").value;
 
   player2 = document.getElementById("player-2").value;
-  
+
   playerScores[player1] = 0;
   playerScores[player2] = 0;
   if (player1.length > 3 && player2.length > 3) {
@@ -172,32 +230,38 @@ const startGameHandler = (e) => {
   } else {
     const span1 = document.getElementById("player1-error");
     const span2 = document.getElementById("player2-error");
-    if (player1.length < 3) {
-      span1.textContent = "Minimum 3 letters in the name and name should be different";
+    if (player1.length < 2) {
+      span1.textContent =
+        "Minimum 3 letters in the name and name should be different";
       span1.style.color = "red";
     }
-    if (player2.length < 3 ) {
-      span2.textContent = "Minimum 3 letters in the name and name should be different";
+    if (player2.length < 2) {
+      span2.textContent =
+        "Minimum 3 letters in the name and name should be different";
       span2.style.color = "red";
     }
-   
-};}
+  }
+};
 
 const battlegroundHandler = async (event) => {
   event.preventDefault();
   const categoryContainer = document.getElementById("category-container");
 
   categories = document.getElementById("categories").value;
+
   if (categories != "choose-category") {
     const forms = document.getElementById("forms");
+    selectedCategories.push(categories);
 
     forms.style.display = "none";
     const battleGround = document.getElementById("battle-ground");
 
     battleGround.classList.remove("battle-ground-h");
     battleGround.classList.add("battle-ground");
+
     quizData = await fetchingQuestions(categories);
-    console.log(quizData)
+
+    console.log(quizData);
 
     questionIndex = 0;
     askNextQuestion();
@@ -206,6 +270,32 @@ const battlegroundHandler = async (event) => {
     span.textContent = "";
 
     span.textContent = "Please choose a category";
+    span.style.color = "red";
+    categoryContainer.append(span);
+  }
+};
+
+const afterNextCategory = async (event) => {
+  event.preventDefault();
+  const endScreen = document.getElementById("endGame");
+  endScreen.classList.add("next-categories-h");
+  endScreen.classList.remove("next-categories");
+  const categoryContainer = document.getElementById("category-container");
+  const selectedCategory = document.getElementById("modifiedCategories").value;
+
+  if (selectedCategory && selectedCategory !== "choose-category") {
+    selectedCategories.push(selectedCategory);
+    const battleGround = document.getElementById("battle-ground");
+    console.log(selectedCategories);
+    battleGround.classList.remove("battle-ground-h");
+    battleGround.classList.add("battle-ground");
+
+    quizData = await fetchingQuestions(selectedCategory);
+    questionIndex = 0;
+    askNextQuestion();
+  } else {
+    const span = document.createElement("span");
+    span.textContent = "Please choose a valid category";
     span.style.color = "red";
     categoryContainer.append(span);
   }
